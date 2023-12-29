57d59ebd06b6176cd2bb86551496c3c67e3f4a87
MrAinslay <asteriospaligiannis@gmail.com>
2023-12-29 10:26:24 +0200

    created repl

--- repl.go
+++ repl.go
@@ -1,5 +1,37 @@
 package main
 
-func startRpl() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func cleanInput(s string) string {
+	output := strings.TrimSpace(s)
+	output = strings.ToLower(output)
+	return output
+}
+
+func printPrompt() {
+	fmt.Print("Pokedex > ")
+}
+
+func startRpl() {
+	commands := getCommands()
+
+	reader := bufio.NewScanner(os.Stdin)
+	printPrompt()
+	for reader.Scan() {
+		text := cleanInput(reader.Text())
+		if command, exists := commands[text]; exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown Command")
+		}
+		printPrompt()
+	}
 }
