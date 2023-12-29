8cfd1c019349b8c00f8fb827169e699a373ffe45
MrAinslay <asteriospaligiannis@gmail.com>
2023-12-29 13:01:15 +0200

    map command

--- command_map.go
+++ command_map.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func commandMap() error {
-	fmt.Println("MAP")
+	type Location struct {
+		Count    int     `json:"count"`
+		Next     *string `json:"next"`
+		Previous *string `json:"previous"`
+		Results  []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"results"`
+	}
+
+	locations := Location{}
+	if locations.Next != nil {
+		err := json.Unmarshal(getAPI(*locations.Next), &locations)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		err := json.Unmarshal(getAPI(""), &locations)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+
+	for _, area := range locations.Results {
+		fmt.Println(area.Name)
+	}
 	return nil
 }
--- http_response.go
+++ http_response.go
@@ -7,18 +7,22 @@ import (
 	"net/http"
 )
 
-func getAPI() {
-	res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
+func getAPI(s string) []byte {
+	link := fmt.Sprintf("https://pokeapi.co/api/v2/location-area/%s", s)
+	res, err := http.Get(link)
 	if err != nil {
 		log.Fatal(err)
+		return nil
 	}
 	body, err := io.ReadAll(res.Body)
 	res.Body.Close()
 	if res.StatusCode > 299 {
 		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		return nil
 	}
 	if err != nil {
 		log.Fatal(err)
+		return nil
 	}
-	fmt.Printf("%v", body)
+	return body
 }
